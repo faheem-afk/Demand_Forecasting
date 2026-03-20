ed8d5107f4f797d73def18a98f3be3a13907596d
= <adahm7114@gmail.com>
2026-03-20 19:55:42 +0000

    finalizing the model library

--- model/app/model_predict.py
+++ model/app/model_predict.py
@@ -1,12 +1,25 @@
+import os
 from meal_demand.ml.predict_model import predict
 from meal_demand.visualization.visuals import show_visuals
 from meal_demand.utils.common import load_data
 from meal_demand.domain.config import Config
 from pathlib import Path
-from postgres import upload_to_postgres
+from postgres import upload_to_postgres, run_query
+
+
+db_args = dict(
+    host = os.getenv("POSTGRES_HOST"),
+    user = os.getenv("POSTGRES_USER"),
+    dbname = os.getenv("POSTGRES_DB"),
+    port = os.getenv("POSTGRES_PORT")
+)
 
 def generate_forecast():
+    table_name = db_args.get("host")
     df = load_data(config=Config(file_path=Path("df_encoded.csv"), artifact_path=Path("../data")))
     df = predict(df)
     show_visuals(df)
-    upload_to_postgres(df, "current_meal_demand")
+    run_query(
+        f"""DROP TABLE IF EXISTS {table_name}""",fetch=False, **db_args
+    )
+    upload_to_postgres(df, table_name, **db_args)
--- model/app/postgres.py
+++ model/app/postgres.py
@@ -1,53 +1,74 @@
 import psycopg2
 from psycopg2 import sql
 from meal_demand.utils.common import get_logger
+import pandas as pd
 
 logger = get_logger()
 
-
-def upload_to_postgres(df, table_name, dbname="postgres", user="postgres", host='meal_demand_postgres', port=5432, upload=False):
-    if upload:
-        logger.info("Uploading to postgres.....")
-        # Connect to PostgreSQL
-        conn = psycopg2.connect(dbname=dbname, user=user, host=host, port=port)
-        cur = conn.cursor()
-        
-        # Create table if it doesn't exist
-        create_table_query = sql.SQL("""
-            CREATE TABLE IF NOT EXISTS {table} (
-                {fields}
-            )
+def upload_to_postgres(df, table_name, dbname="postgres", user="postgres", host='localhost', port=5432):
+ 
+    logger.info("Uploading to postgres.....")
+    # Connect to PostgreSQL
+    conn = psycopg2.connect(dbname=dbname, user=user, host=host, port=port)
+    cur = conn.cursor()
+    
+    # Create table if it doesn't exist
+    create_table_query = sql.SQL("""
+        CREATE TABLE IF NOT EXISTS {table} (
+            {fields}
+        )
+    """).format(
+        table=sql.Identifier(table_name),
+        fields=sql.SQL(', ').join(
+            sql.SQL("{} {}").format(sql.Identifier(col), sql.SQL(pandas_to_postgres_type(str(df[col].dtype)))) for col in df.columns
+        )
+    )
+    cur.execute(create_table_query)
+    conn.commit()
+    # Insert data into table
+    for index, row in df.iterrows():
+        insert_query = sql.SQL("""
+            INSERT INTO {table} ({fields}) VALUES ({values})
         """).format(
             table=sql.Identifier(table_name),
-            fields=sql.SQL(', ').join(
-                sql.SQL("{} {}").format(sql.Identifier(col), sql.SQL(pandas_to_postgres_type(str(df[col].dtype)))) for col in df.columns
-            )
+            fields=sql.SQL(', ').join(map(sql.Identifier, df.columns)),
+            values=sql.SQL(', ').join(sql.Placeholder() * len(df.columns))
         )
-        cur.execute(create_table_query)
-        conn.commit()
-        # Insert data into table
-        for index, row in df.iterrows():
-            insert_query = sql.SQL("""
-                INSERT INTO {table} ({fields}) VALUES ({values})
-            """).format(
-                table=sql.Identifier(table_name),
-                fields=sql.SQL(', ').join(map(sql.Identifier, df.columns)),
-                values=sql.SQL(', ').join(sql.Placeholder() * len(df.columns))
-            )
-            cur.execute(insert_query, tuple(row))
-        
-        conn.commit()
+        cur.execute(insert_query, tuple(row))
+    
+    conn.commit()
+    cur.close()
+    conn.close()
+    logger.info("....Successful.")
+
+def pandas_to_postgres_type(dtype):
+    if dtype == 'int64':
+        return 'INTEGER'
+    elif dtype == 'float64':
+        return 'FLOAT'
+    elif dtype == 'bool':
+        return 'BOOLEAN'
+    else:
+        return 'TEXT'
+
+
+def run_query(query, fetch=True, dbname="postgres", user="postgres", host="localhost", port=5432):
+    conn = psycopg2.connect(dbname=dbname, user=user, host=host, port=port)
+    cur = conn.cursor()
+
+    logger.info("Dropping the older table...")
+    cur.execute(query)
+    conn.commit()
+    logger.info("Older Table dropped Successfully.")
+
+    if fetch:
+        rows = cur.fetchall()
+        colnames = [desc.name for desc in cur.description]
+
         cur.close()
         conn.close()
-        logger.info("....Successful.")
 
-    def pandas_to_postgres_type(dtype):
-        if dtype == 'int64':
-            return 'INTEGER'
-        elif dtype == 'float64':
-            return 'FLOAT'
-        elif dtype == 'bool':
-            return 'BOOLEAN'
-        else:
-            return 'TEXT'
-        
+        df = pd.DataFrame(rows, columns = colnames)
+
+        return df
+    
